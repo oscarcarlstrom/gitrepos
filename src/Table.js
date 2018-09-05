852d558249f65ad937e3621fcfb5cce74ae072c7
oscarcarlstrom <oscar.carlstrom@gmail.com>
2018-09-05 16:48:56 +0200

    Added momentJS (npm package)

--- src/Table.js
+++ src/Table.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import './Table.min.css';
 
 class Table extends Component {
@@ -22,7 +23,7 @@ class Table extends Component {
             { item.description }
           </td>
           <td className="nobreak">
-            { item.created_at }
+            { moment(item.created_at).format("YYYY-MM-DD") }
           </td>
           <td className="number">
             { item.stargazers_count } <i className="fas fa-star" aria-hidden="true"></i>
